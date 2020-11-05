f8a08f49f6ddbfa2e3d3a7dec7ed0e1d2c989928
Hans Melkerud <hans.melkerud@bahnhof.se>
2020-11-05 18:44:40 +0100

    Enable prometheus deployment and service

--- apply-all.sh
+++ apply-all.sh
@@ -2,6 +2,6 @@
 
 kubectl apply -f webservice.deployment.yml
 kubectl apply -f webservice.service.yml
-# kubectl apply -f prometheus.deployment.yml
-# kubectl apply -f prometheus.service.yml
+kubectl apply -f prometheus.deployment.yml
+kubectl apply -f prometheus.service.yml
 kubectl apply -f frontend.service.yml
