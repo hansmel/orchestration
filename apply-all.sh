4acbcaebd9ce99e5a993347c3ed4e87cd489f7e1
Hans Melkerud <hans.melkerud@bahnhof.se>
2020-11-05 16:52:11 +0100

    Add prometheus deployment and service (out-commented)

--- apply-all.sh
+++ apply-all.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 kubectl apply -f webservice.deployment.yml
+kubectl apply -f prometheus.deployment.yml
 kubectl apply -f webservice.service.yml
 kubectl apply -f frontend.service.yml
--- delete-all.sh
+++ delete-all.sh
@@ -2,4 +2,6 @@
 
 kubectl delete -f webservice.deployment.yml 
 kubectl delete -f webservice.service.yml
+# kubectl delete -f prometheus.deployment.yml 
+# kubectl delete -f prometheus.service.yml
 kubectl delete -f frontend.service.yml
