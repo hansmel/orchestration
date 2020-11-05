0675846fea8fc0bd704600c581bfdd16b2339df8
Hans Melkerud <hans.melkerud@bahnhof.se>
2020-11-05 16:18:34 +0100

    Change to webservice deployment

--- apply-all.sh
+++ apply-all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-kubectl apply -f webservice.pod.yml
+kubectl apply -f webservice.deployment.yml
 kubectl apply -f webservice.service.yml
 kubectl apply -f frontend.service.yml
--- delete-all.sh
+++ delete-all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-kubectl delete -f webservice.pod.yml 
+kubectl delete -f webservice.deployment.yml 
 kubectl delete -f webservice.service.yml
 kubectl delete -f frontend.service.yml
