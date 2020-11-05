046b8f7c6c1794ed7351c0ef967492dd3ca46767
Hans Melkerud <hans.melkerud@bahnhof.se>
2020-11-05 16:57:09 +0100

    Disable prometheus deployment and service

--- apply-all.sh
+++ apply-all.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 kubectl apply -f webservice.deployment.yml
-kubectl apply -f prometheus.deployment.yml
 kubectl apply -f webservice.service.yml
+# kubectl apply -f prometheus.deployment.yml
+# kubectl apply -f prometheus.service.yml
 kubectl apply -f frontend.service.yml
