a76c2b344859e87d9ce0d88494fd5628cc6e4f65
Hans Melkerud <hans.melkerud@bahnhof.se>
2020-11-05 14:36:16 +0100

    Update file names

--- apply-all.sh
+++ apply-all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-kubectl apply -f webservice-pod.yml
-kubectl apply -f webservice-service.yml
+kubectl apply -f webservice.pod.yml
+kubectl apply -f webservice.service.yml
--- delete-all.sh
+++ delete-all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-kubectl delete -f webservice-pod.yml 
-kubectl delete -f webservice-service.yml 
+kubectl delete -f webservice.pod.yml 
+kubectl delete -f webservice.service.yml
